3676d8f05d2b129eb2d6c094809b3814098654b2
sansSpoon <sans.spoon@gmail.com>
2017-09-05 14:37:36 +1000

    layering, margins and overflox fixed

--- assets/js/ux.js
+++ assets/js/ux.js
@@ -5,3 +5,28 @@ var	controlButton = document.querySelector('.polygon'),
 controlButton.onclick = function() {
 	controlStrip.classList.toggle('stripClosed');
 };
+
+
+//window.onscroll = function() {
+//	var viewportOffset = document.querySelector('.post-meta').getBoundingClientRect().top;
+//    console.log(viewportOffset);
+//};
+
+
+
+/*
+var myElement = document.querySelector('.post-meta');
+
+function getElementTop(el) {
+	var eTop = el.getBoundingClientRect().top;
+	return eTop;
+}
+
+window.onscroll = function() {
+	var scrollingEl = getElementTop(myElement);
+	
+	if (scrollingEl == 0) {
+		console.log("poop");
+	}
+};
+*/
