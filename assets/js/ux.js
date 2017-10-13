6605f784ddcc4b7e64afc223da3c9dd21a9e661a
sansSpoon <sans.spoon@gmail.com>
2017-10-13 21:25:24 +1100

    made modular tag style. fixed tag alignment.

--- assets/js/ux.js
+++ assets/js/ux.js
@@ -2,7 +2,8 @@
 var	controlButton = document.getElementById('neo-n-s'),
 	controlStrip = document.querySelector('.strip'),
 	csWidth = controlStrip.getBoundingClientRect().width,
-	csState = "open";
+	csState = "open",
+	apTags = document.getElementsByClassName('post-tags');
 
 function csOpen() {
 	controlStrip.style.removeProperty('transform');
@@ -13,6 +14,32 @@ function csClose() {
 	controlStrip.style.WebkitTransform = 'translate(-' + csWidth + 'px)';
 	controlStrip.style.transform = 'translate(-' + csWidth + 'px)';
 	csState = "closed";
+}
+
+function tagExpand(event) {
+	if (this.dataset.tag) {
+		if (event.type == 'mouseover') {
+			myTag = this.innerHTML;
+			this.innerHTML = this.dataset.tag;
+		} else if (event.type == 'mouseout') {
+			this.innerHTML = myTag;
+		}
+	}
+}
+
+function alignTags(apTags) {
+	console.log(apTags);
+	for (var i=0; i<apTags.length; i++) { 
+		
+		postTagsLoc = apTags[i].offsetLeft;
+		console.log(postTagsLoc);
+		if (postTagsLoc == 0) {
+			apTags[i].getElementsByTagName('li')[0].className = 'first-tag';
+		} else {
+			console.log(apTags[i]);
+			apTags[i].getElementsByTagName('li')[0].className = 'tag';
+		}
+	}
 }
 
 controlButton.onclick = function() {
@@ -48,30 +75,35 @@ window.addEventListener("optimizedResize", function() {
 	csWidth = controlStrip.getBoundingClientRect().width;
 	if (csState == "closed") {
 		csClose();
-	}	
+	}
+	
+	alignTags(apTags);
+	
+	
 });
 
-
-var pTags = document.querySelector('.post-tags');
-if (pTags) {
-	tags = pTags.getElementsByTagName('li');
-	
-	for (var i=0; i<tags.length; i++) {
-		tags[i].firstChild.addEventListener('mouseover', tagExpand, false);
-		tags[i].firstChild.addEventListener('mouseout', tagExpand, false);
-	}
-	
-	function tagExpand(event) {
-		if (this.dataset.tag) {
-			if (event.type == 'mouseover') {
-				myTag = this.innerHTML;
-				this.innerHTML = this.dataset.tag;
-			} else if (event.type == 'mouseout') {
-				this.innerHTML = myTag;
-			}
+if (apTags) {
+	for (var i=0; i<apTags.length; i++) { 
+		
+		tags = apTags[i].getElementsByTagName('li');
+		
+		for (var j=0; j<tags.length; j++) {
+			tags[j].firstChild.addEventListener('mouseover', tagExpand, false);
+			tags[j].firstChild.addEventListener('mouseout', tagExpand, false);
 		}
+		
 	}
 }
+
+/*
+var childPos = obj.offset();
+var parentPos = obj.parent().offset();
+var childOffset = {
+    top: childPos.top - parentPos.top,
+    left: childPos.left - parentPos.left
+}
+*/
+
 
 //window.onscroll = function() {
 //	var viewportOffset = document.querySelector('.post-meta').getBoundingClientRect().top;
